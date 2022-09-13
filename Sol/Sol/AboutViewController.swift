8a262a1682fa87af3b06238db3ec33ac92508eb7
levigroker <levigroker@gmail.com>
2022-09-13 00:41:33 -0600

    Add UI for About view
    Signed-off-by: levigroker <levigroker@gmail.com>

---
+++ Sol/Sol/AboutViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AboutViewController.swift
+//  Sol
+//
+//  Created by Levi Brown on 2022-09-12.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
